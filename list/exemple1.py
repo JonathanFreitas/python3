097e7b6aa480e8e713662da025ee80d5eb0ecede
jonathanfreitas <jonathanfreitassouza28@gmail.com>
2021-09-02 15:34:52 -0300

    Create exemple1.py

---
+++ list/exemple1.py
@@ -0,0 +1,16 @@
+type([])
+
+list1 = [1,2,3,4,5,6,7]
+
+list2 = ['S','C','H','O','O','L']
+
+list3 = []
+
+list4 = list(range(11))
+
+list5 = list('University')
+
+if 8 in list4:
+	print('find number 8')
+else:
+	print('No find number 8')
