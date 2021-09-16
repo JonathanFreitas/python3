fdc4eaa7758ef41db5e01216e61cf31adf570e11
JonathanFreitas <34845116+JonathanFreitas@users.noreply.github.com>
2021-09-16 17:28:19 -0300

    Add files via upload

---
+++ tuple/exemplo1.py
@@ -0,0 +1,17 @@
+print(type(()))
+
+tupla1 = (1, 2, 3)
+print(tupla1)
+print(type(tupla1))
+
+tupla2 = 1, 2, 3
+print(tupla2)
+print(type(tupla2))
+
+tupla3 = (4) # isso não e uma tupla
+print(tupla3)
+print(type(tupla3))
+
+tupla4 = (4,) # Tupla
+print(tupla4)
+print(type(tupla4))
